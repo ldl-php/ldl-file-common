71a5e038342ffa3b215c3fbd5c59d9e627dffdea
Federico Stange <jpfstange@gmail.com>
2022-03-10 12:40:02 +0000

    feature/1201948369036945 - Add DirectoryCollection::getRelativePaths() : iterable

--- src/File/Collection/Contracts/DirectoryCollectionInterface.php
+++ src/File/Collection/Contracts/DirectoryCollectionInterface.php
@@ -1,19 +1,24 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace LDL\File\Collection\Contracts;
 
+use LDL\File\Exception\FileExistsException;
 use LDL\Framework\Base\Contracts\ArrayFactoryInterface;
 use LDL\Framework\Base\Contracts\IterableFactoryInterface;
 use LDL\Type\Collection\TypedCollectionInterface;
 
 /**
- * Interface DirectoryCollectionInterface
- * @package LDL\File\Collection\Contracts
- *
- * There are no custom methods *yet*
+ * Interface DirectoryCollectionInterface.
  */
 
 interface DirectoryCollectionInterface extends TypedCollectionInterface, IterableFactoryInterface, ArrayFactoryInterface
 {
-
+    /**
+     * Yields each directory's real path in the collection.
+     *
+     * @throws FileExistsException
+     */
+    public function getRealPaths(): iterable;
 }
